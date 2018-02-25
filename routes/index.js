9a85d4e0b7385a7ed7fff80a36138f479d024547
Toresa <Toresa@users.noreply.github.com>
2018-02-25 09:52:43 +0100

    Bootstrap ui added
    Added a bootstrap template to make it look a bit better

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Node-passport-sqlite example' });
+    res.render('index', { title: 'Node-passport-sqlite example', user:{} });
 });
 
 module.exports = router;
